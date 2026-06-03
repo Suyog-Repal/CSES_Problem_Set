95dbacf4e03f9c7e4636f63f0dc123db31fa50e1
Suyog Repal <suyog.repal23@spit.ac.in>
2026-06-03 22:08:10 +0530

    solved Number Spiral Problem

---
+++ Introductory Problems/Number_Spiral.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std; 
+using ll = long long; 
+
+int main(){
+    ll t; 
+    cin >> t; 
+    ll x; 
+    ll y; 
+    for(ll i = 0; i<t; i++){
+       cin >> x >> y; 
+       if(y>x){
+        if(y%2==1){
+            cout << y*y - (x-1) << "\n";
+        }else{
+            cout << (y-1)*(y-1) + x << "\n"; 
+        }
+       }else{
+        if(x%2 == 0){
+            cout << x*x - (y-1) << "\n";
+        }else{
+            cout << (x-1)*(x-1)  + y << "\n";
+        }
+       }
+    }
+    return 0; 
+}
