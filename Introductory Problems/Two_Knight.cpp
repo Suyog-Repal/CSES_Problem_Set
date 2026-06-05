d19499e8df60bcccab65fa6dedb940916f89a46f
Suyog Repal <suyog.repal23@spit.ac.in>
2026-06-05 09:22:35 +0530

    updated two knight solution

--- Introductory Problems/Two_Knight.cpp
+++ Introductory Problems/Two_Knight.cpp
@@ -4,14 +4,15 @@ using ll = long long;
 
 int main(){
     ios::sync_with_stdio(false); 
-    cin.tie(nullptr);
+    cin.tie(nullptr); 
 
-    ll t;
+    ll t; 
     cin >> t; 
     for(ll k = 1; k<=t; k++){
         ll total = k*k*(k*k - 1)/2; 
         ll attack = 4*(k-1)*(k-2); 
-        cout << total - attack << '\n'; 
+        cout << total - attack << '\n';
     }
+    
     return 0; 
 }
