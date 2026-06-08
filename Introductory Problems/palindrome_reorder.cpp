26a79b1d43ca1de820029c2ab728d698f6d30e4a
Suyog Repal <suyog.repal23@spit.ac.in>
2026-06-08 11:24:59 +0530

    Solved Palindrome reorder problem - Introductory Problem

---
+++ Introductory Problems/palindrome_reorder.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std; 
+using ll = long long; 
+using vll = vector<ll>; 
+
+int main(){
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr); 
+
+    string s; 
+    cin >> s; 
+    unordered_map<char, int> freq;  
+    
+    for(char c: s) freq[c]++; 
+    // check does form the palindrom or not: 
+    ll odd = 0; 
+    for(auto &p: freq){
+      if(p.second%2 == 1) odd++; 
+    }
+    if(odd > 1){
+        cout << "NO SOLUTION\n"; 
+        return 0; 
+    }
+    string left = ""; 
+   for(auto &p: freq){
+      char ch = p.first; 
+      ll count = p.second; 
+      left += string(count/2, ch); 
+   }
+   string right = left; 
+   reverse(right.begin(), right.end()); 
+   string middle = ""; 
+   for(auto &p: freq){
+    char ch = p.first; 
+    if(p.second%2 == 1) middle += string(1, ch); 
+   }
+   cout << left + middle + right << endl; 
+    return 0; 
+}
