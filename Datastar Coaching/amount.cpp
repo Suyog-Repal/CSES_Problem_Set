6a531f1259c4aa11f2d63bd5b4549c733012a0f9
Suyog Repal <suyog.repal23@spit.ac.in>
2026-06-15 15:12:59 +0530

    solved amount problem from Datastar Coaching

---
+++ Datastar Coaching/amount.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+using ll = long long; 
+
+int main(){
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr); 
+    ll a, b; 
+    cin >> a >> b; 
+    ll sum = a + b; 
+    cout << sum << '\n'; 
+    return 0; 
+}
