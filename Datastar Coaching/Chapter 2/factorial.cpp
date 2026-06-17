4ff133352f2ed299feed12cefed8e5b6c134272b
Suyog Repal <suyog.repal23@spit.ac.in>
2026-06-17 21:13:17 +0530

    Solved factorial - datastar coaching/chapter 2

---
+++ Datastar Coaching/Chapter 2/factorial.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+using ll = long long; 
+using vll = vector<long long>; 
+
+int main(){
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr); 
+    
+    ll n; 
+    cin >> n; 
+    ll count = 0; 
+    while(n>1){
+        n = n/5; 
+        count += n; 
+    }
+    cout << count << '\n';  
+    return 0; 
+}
